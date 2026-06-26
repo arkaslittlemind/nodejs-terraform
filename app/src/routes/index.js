be2aa19b17c197780bff84897e515e0f036f1471
Arkadipta Mojumder <arkadiptamojumder14@gmail.com>
2026-06-27 02:05:27 +0530

    express app setup

---
+++ app/src/routes/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to the Node.js DevOps Project!",
+  });
+});
+
+router.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "UP",
+    timestamp: new Date().toISOString(),
+  });
+});
+
+module.exports = router;
